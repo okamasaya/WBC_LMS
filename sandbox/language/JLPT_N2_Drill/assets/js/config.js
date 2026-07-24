b276ff7aa7018a089fdf42bec1dcad04fc438ae9
okamasaya <okamasaya.devwork@gmail.com>
2026-07-24 15:43:39 +0900

    Update config.js
    (little)bug fix

--- sandbox/language/JLPT_N2_Drill/assets/js/config.js
+++ sandbox/language/JLPT_N2_Drill/assets/js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     drillFiles: [
         "JLPT_N2_Drill_0.html",
         "JLPT_N2_Drill_1.html",
-        "JLPT_N2_Drill_2.html"
+        "JLPT_N2_Drill_2.html",
         "JLPT_N2_Drill_3.html"
     ]
 };
