1f963ffa46d052ecf04b357bda2ddb7470baca1c
okamasaya <okamasaya.devwork@gmail.com>
2026-07-30 19:52:17 +0900

    move config.js for JLPT_N2_Drill/

---
+++ sandbox/language/JLPT_N2_Drill/config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    drillFiles: [
+        "jlpt_n2_drill_0.html",
+        "jlpt_n2_drill_1.html",
+        "jlpt_n2_drill_2.html",
+        "jlpt_n2_drill_3.html",
+        "jlpt_n2_drill_4.html",
+        "jlpt_n2_drill_5.html",
+        "jlpt_n2_drill_6.html",
+        "jlpt_n2_drill_7.html",
+        "jlpt_n2_drill_8.html",
+        "jlpt_n2_drill_9.html",
+        "jlpt_n2_drill_10.html"
+    ]
+};
