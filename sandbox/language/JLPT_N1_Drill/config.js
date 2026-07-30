fa3ba473cd45dd6133a42decfc38a008366ab424
okamasaya <okamasaya.devwork@gmail.com>
2026-07-30 19:54:41 +0900

    new update file JLPT_N1_Drill

---
+++ sandbox/language/JLPT_N1_Drill/config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    drillFiles: [
+        "jlpt_n1_drill_0.html",
+        "jlpt_n1_drill_1.html",
+        "jlpt_n1_drill_2.html",
+        "jlpt_n1_drill_3.html",
+        "jlpt_n1_drill_4.html",
+        "jlpt_n1_drill_5.html",
+        "jlpt_n1_drill_6.html",
+        "jlpt_n1_drill_7.html",
+        "jlpt_n1_drill_8.html",
+        "jlpt_n1_drill_9.html",
+        "jlpt_n1_drill_10.html"
+    ]
+};
