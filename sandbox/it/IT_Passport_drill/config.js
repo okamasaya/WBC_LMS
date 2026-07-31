5ac19b1120700d3430c7d44a709c4c63c57adbd8
okamasaya <okamasaya.devwork@gmail.com>
2026-07-31 15:37:56 +0900

    Add IT PASSPORT drill

---
+++ sandbox/it/IT_Passport_drill/config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    drillFiles: [
+        "it_passport_drill_0.html",
+        "it_passport_drill_1.html",
+        "it_passport_drill_2.html",
+        "it_passport_drill_3.html",
+        "it_passport_drill_4.html",
+        "it_passport_drill_5.html",
+        "it_passport_drill_6.html",
+        "it_passport_drill_7.html",
+        "it_passport_drill_8.html",
+        "it_passport_drill_9.html",
+        "it_passport_drill_10.html"
+    ]
+};
