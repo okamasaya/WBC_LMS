0771d09e8110a14e2dce5cc63e940782335ae614
okamasaya <okamasaya.devwork@gmail.com>
2026-08-17 07:45:27 +0900

    update for new version in JLPT_N2

---
+++ prototype/language/JLPT_N2/config.js
@@ -0,0 +1,23 @@
+const CONFIG = {
+    drillFiles: [
+        "it_passport_drill_0.html",
+        "it_passport_drill_1.html",
+        "it_passport_drill_2.html",
+        "it_passport_drill_3.html",
+        "it_passport_drill_4.html",
+        "it_passport_drill_5.html",
+        "it_passport_drill_6.html",
+        "it_passport_drill_7.html",
+        "it_passport_drill_8.html",
+        "it_passport_drill_9.html",
+        "it_passport_drill_10.html",
+        "it_passport_drill_11.html"
+    ],
+
+    // new version
+    questionFile: ["./data/JLPT_LK_N2_Ch.json","./data/JLPT_LK_N2_Vo.json"],
+    questionCount: 20,
+    randomQuestions: true
+
+};
+
