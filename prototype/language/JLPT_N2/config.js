c7a3401fe502f823d8998dff7acea2078204cd89
okamasaya <okamasaya.devwork@gmail.com>
2026-08-18 08:08:14 +0900

    Update config.js

--- prototype/language/JLPT_N2/config.js
+++ prototype/language/JLPT_N2/config.js
@@ -15,8 +15,8 @@ const CONFIG = {
     ],
 
     // new version
-    questionFile: ["./data/JLPT_N2_LK_Ch.json"
-                   ],
+    questionFile: "./data/JLPT_N2_LK_Ch.json",
+                  
     questionCount: 20,
     randomQuestions: true
 
